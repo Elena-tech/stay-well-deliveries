8c2e255e57864d698a3c58fb364bbd6e9c080920
Gonzalo Diaz <info@ventra.com.co>
2020-03-16 13:30:33 +0000

    added routes, some styling sheets, fonts and colours

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/donate', to: 'pages#donate'
+    get '/help', to: 'pages#help'
+    get '/collaborator', to: 'pages#collaborator'
+    get '/contact', to: 'pages#contact'
 end
