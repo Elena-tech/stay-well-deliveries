de7d56e33476f4c9e203593572a61d9c891c135d
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-03-17 17:19:19 +0100

    adding tabs to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     get '/help', to: 'pages#help'
     get '/collaborator', to: 'pages#collaborator'
     get '/contact', to: 'pages#contact'
+    get '/dashboard', to: 'pages#dashboard'
 end
